7ac639036bd9fc3222397fa9a234ba159be19a27
motofumi-410 <always.moto410@gmail.com>
2025-04-16 19:56:21 +0900

    prohibitedArea
    危険区域を作成

---
+++ TORICA sim Develop/Assets/Script/prohibitedArea.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class prohibitedArea : MonoBehaviour
+{
+    private GameObject Result;
+    [SerializeField] private Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Result = GameObject.Find("Result");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        MyGameManeger.instance.Landing = true;
+        MyGameManeger.instance.SettingMode = 0;
+        Debug.Log("AAAAAAA");
+        canvas.enabled = true;
+        Time.timeScale=(float)Convert.ToInt32(!MyGameManeger.instance.SettingActive & !MyGameManeger.instance.Landing);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Result.SetActive(!MyGameManeger.instance.SettingActive & MyGameManeger.instance.Landing);
+    }
+}
