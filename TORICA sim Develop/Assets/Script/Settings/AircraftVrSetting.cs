e9c9ef827d4e503be2d316279767fb5c7b683f7d
Yuta Tanaka <xpyuuta.02.05@gmail.com>
2025-07-09 21:38:00 +0900

    yes!!!!!!!!!!!!!!!!!!!!!

--- TORICA sim Develop/Assets/Script/Settings/AircraftVrSetting.cs
+++ TORICA sim Develop/Assets/Script/Settings/AircraftVrSetting.cs
@@ -8,23 +8,40 @@ public class AircraftVrSetting : MonoBehaviour
     private GameObject VRModeObjects;
     [SerializeField] private GameObject NonVRModeObjects;
     private GameObject obj;
+    private bool VREnable;
 
     void Start()
     {
+        #if USE_STEAMVR
+        VREnable = true;
+        MyGameManeger.instance.VRMode = true;
+        // ここにVR用のコントローラー設定などのコードを記述
+        #else
+        // USE_STEAMVR シンボルが定義されていない（＝SteamVRが無効な）場合の処理
+        VREnable = false;
+        MyGameManeger.instance.VRMode = false;
+        // ここにPC用のカメラ設定や入力設定のコードを記述
+        #endif
+
+
         nowSetting = MyGameManeger.instance.VRMode;
         VRModeObjects = (GameObject)Resources.Load("VR_Item");
 
-        if (nowSetting)
+        if (nowSetting & VREnable)
         {
             //VRモード
             obj = Instantiate(VRModeObjects, new Vector3(VRModeObjects.transform.position.x,VRModeObjects.transform.position.y,VRModeObjects.transform.position.z), Quaternion.identity);
+            Debug.Log(VRModeObjects.transform.position.y);
             NonVRModeObjects.SetActive(false);
             obj.transform.parent = MyGameManeger.instance.Plane.transform;
+            obj.transform.localPosition = new Vector3(obj.transform.position.x,obj.transform.position.y,obj.transform.position.z);
         }
         else
         {
             //非VRモード
-            Destroy(obj);
+            if(VREnable){
+                Destroy(obj);
+            }
             NonVRModeObjects.SetActive(true);
         }
     }
@@ -32,7 +49,7 @@ public class AircraftVrSetting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (nowSetting != MyGameManeger.instance.VRMode)
+        if (nowSetting != MyGameManeger.instance.VRMode & VREnable)
         {
             nowSetting = MyGameManeger.instance.VRMode;
             if (nowSetting)
@@ -41,11 +58,14 @@ public class AircraftVrSetting : MonoBehaviour
                 obj = Instantiate(VRModeObjects, new Vector3(VRModeObjects.transform.position.x,VRModeObjects.transform.position.y,VRModeObjects.transform.position.z), Quaternion.identity);
                 NonVRModeObjects.SetActive(false);
                 obj.transform.parent = MyGameManeger.instance.Plane.transform;
+                obj.transform.localPosition = new Vector3(obj.transform.position.x,obj.transform.position.y,obj.transform.position.z);
             }
             else
             {
                 //非VRモード
-                Destroy(obj);
+                if(VREnable){
+                    Destroy(obj);
+                }                
                 NonVRModeObjects.SetActive(true);
             }
         }
